3c84d5aba55ffc7fd54cc40da9f70de4a14f5311
RajveerSingh1321 <rajveersingh391717@gmail.com>
2025-11-24 19:25:07 +0530

    updatedresume

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/RajveerSingh1321",
   resume:
-    "https://drive.google.com/file/d/14uhZZPrZP-kOitYouTdwzI53GraJ5-S5/view?usp=sharing",
+    "https://drive.google.com/file/d/13fKrKSJxwbyQdQhjDxt4YRont8fJ-7DJ/view?usp=sharing",
   linkedin: "www.linkedin.com/in/rajveersingh1321",
   twitter: "https://twitter.com/Rajveer132109",
   insta: "https://www.instagram.com/rajveer_singh_9f/",
